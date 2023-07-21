6f352cc4db5224e0742401316fdef4db0d76f1f3
olasoMateus <73443539+olasoMateus@users.noreply.github.com>
2023-07-21 06:53:00 -0300

    Fix

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ from src.controllers.tripController import *
 #     # A welcome message to test our server
 #     return "<h1>Welcome to our medium-greeting-api!</h1>"
 
-server.run()
+# server.run()
 
 app = server.app
